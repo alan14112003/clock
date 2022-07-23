52026827b043e83808985afa94c9941aea24811d
alan14112003 <sonnn.21it@gmil.com>
2022-07-24 03:28:13 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const clockBoxHours = document.querySelector('.clockBox__hours');
+const clockBoxMinutes = document.querySelector('.clockBox__minute');
+const clockBoxSeconds = document.querySelector('.clockBox__second');
+
+//  xử lí sự kiện laod 
+const loader = document.querySelector('.container-loader');
+window.onload = () => {
+    loader.style.display = 'none';
+    document.querySelector('.clock-container').style.display = 'flex';
+}
+// xử lí đồng hồ 
+let date = new Date();
+let seconds, minutes, hours;
+seconds = date.getSeconds();
+minutes = date.getMinutes();
+hours = date.getHours();
+clockBoxSeconds.style = 'transform: translate(-50%, -50%) rotate(' + seconds * 6 + 'deg)';
+clockBoxMinutes.style = 'transform: translate(-50%, -50%) rotate(' + minutes * 6 + 'deg)';
+clockBoxHours.style = 'transform: translate(-50%, -50%) rotate(' + hours * 30 + 'deg)';
+setInterval(() => {
+    if (seconds === 59) {
+        seconds = 0;
+        minutes ++;
+        clockBoxMinutes.style = 'transform: translate(-50%, -50%) rotate(' + minutes * 6 + 'deg)';
+    }
+    else
+        seconds++;
+    if (minutes === 60 ) {
+        minutes = 0;
+        hours ++;
+        clockBoxHours.style = 'transform: translate(-50%, -50%) rotate(' + hours * 30 + 'deg)';
+    }
+    if (hours === 60) {
+        hours = 0;
+    }
+    clockBoxSeconds.style = 'transform: translate(-50%, -50%) rotate(' + seconds * 6 + 'deg)';
+    console.log(new Date().getHours() +' '+ new Date().getMinutes() +' '+ new Date().getSeconds());
+}, 1000);
